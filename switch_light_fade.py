7224796841d715d0e837ed8b27c7a167d9e1047d
adriankoehler <root@raspberrypi.(none)>
2018-01-12 00:00:17 +0100

    finished switch-light

--- switch_light_fade.py
+++ switch_light_fade.py
@@ -7,8 +7,9 @@
 import time
 import os
 import sys
+import math
 
-STEPS = 100
+STEPS = 25
 DELAY = float(sys.argv[1])
 PINred = 18
 PINgreen = 23
@@ -19,17 +20,16 @@ Rs,Gs,Bs = (0.0, 0.0, 0.0)
 Re,Ge,Be = (0.0, 0.0, 0.0)
 
 def pwm(r, g, b):
-	print "switchFade: r[" + str(r) + "]g[" + str(g) + "]b[" + str(b) + "]"
+	#print "switchFade: r[" + str(r) + "]g[" + str(g) + "]b[" + str(b) + "]"
 	#cmd = "echo " + str(PINred) + "=" + str(r) + " > /dev/pi-blaster;" + "echo " + str(PINgreen) + "=" + str(r) + " > /dev/pi-blaster;" + "echo " + str(PINblue) + "=" + str(b) + " > /dev/pi-blaster;"
 	cmd = "echo " + str(PINred) + "=" + str(r) + " _ " + str(PINgreen) + "=" + str(g) + " _ " + str(PINblue) + "=" + str(b)
 	os.system(cmd)
 	time.sleep(DELAY)
 
 #übergebene parameter auslesen und in r,g,b variablen abspeichern/umwandeln
-colors = {'white': (1.0, 1.0, 1.0), 'red': (1.0, 0.0, 0.0), 'green': (0.0, 1.0, 0.0), 'blue': (0.0, 1.0, 0.0), 'cyan': (0.0, 1.0, 1.0)}
+#TODO: add colors
+colors = {'white': (1.0, 1.0, 1.0), 'red': (1.0, 0.0, 0.0), 'green': (0.0, 1.0, 0.0), 'blue': (0.0, 0.0, 1.0), 'cyan': (0.0, 1.0, 1.0)}
 (Rs, Gs, Bs) = colors.get(farbeStart, (0.0, 0.0, 0.0))
-#TODO: add colors; copy into RGBe assignment
-colors = {'white': (1.0, 1.0, 1.0), 'red': (1.0, 0.0, 0.0), 'green': (0.0, 1.0, 0.0), 'blue': (0.0, 1.0, 0.0), 'cyan': (0.0, 1.0, 1.0)}
 (Re, Ge, Be) = colors.get(farbeEnd, (0.0, 0.0, 0.0))
 
 diffrenceR = Rs - Re
@@ -44,10 +44,11 @@ print "RGBe: r[" + str(Re) + "]g[" + str(Ge) + "]b[" + str(Be) + "]"
 print "current: r[" + str(currentR) + "]g[" + str(currentG) + "]b[" + str(currentB) + "]"
 
 pwm(currentR, currentG, currentB)
-#eins von beiden immer gleich
-while (currentR!=Re and currentG!=Ge and currentB!=Be) :
-	print "test?"
+
+for i in xrange(1, STEPS-1):
 	currentR -= diffrenceR/STEPS
 	currentG -= diffrenceG/STEPS
 	currentB -= diffrenceB/STEPS
 	pwm(currentR, currentG, currentB)
+
+pwm(Re, Ge, Be)
